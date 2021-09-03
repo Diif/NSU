94883b48f4f8bf1ff26bcf09c837f557dd694069
Diif <30970636+Diif@users.noreply.github.com>
2021-09-03 23:28:43 +0700

    0a is ready

---
+++ T1/0a/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kiwul/Documents/projects/NSU C++/T1/0a
+# Build directory: C:/Users/kiwul/Documents/projects/NSU C++/T1/0a/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
