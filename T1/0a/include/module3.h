ae0bc1b319ba025b277ac836b0d4bf753898787f
Diif <30970636+Diif@users.noreply.github.com>
2021-09-04 22:47:21 +0700

    Remove old folder

--- T1/0a/include/module3.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-namespace Module3 {
-std::string GetMyName();
-}
--- T1/0a/src/module3.cpp
+++
@@ -1,7 +0,0 @@
-#include "module3.h"
-namespace Module3 {
-std::string GetMyName() {
-  std::string name = "Peter";
-  return name;
-}
-}  // namespace Module3
