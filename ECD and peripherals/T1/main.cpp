a5e5bb5ced32f304ac4f50b3a20a312a94ef5a15
WSL Debian Diif <prozella@DESKTOP-O5EA561.localdomain>
2021-09-11 23:28:49 +0700

    gitignore update and ECD lab1 timers

--- ECD and peripherals/T1/main.cpp
+++ ECD and peripherals/T1/main.cpp
@@ -1,10 +1,28 @@
+#include <sys/times.h>
+#include <unistd.h>
+
 #include <cmath>
 #include <iostream>
+
+using namespace std;
 
 double MathFunction(double x);
 double CalculateIntegral(int num_of_interval);
 
-int main(int, char**) { std::cout << CalculateIntegral(10); }
+int main(int, char**) {
+  struct tms start, end;
+  long clocks_per_sec = sysconf(_SC_CLK_TCK);
+  long clocks;
+  cout.precision(20);
+
+  times(&start);
+  cout << CalculateIntegral(100000000) << endl;
+  times(&end);
+
+  clocks = end.tms_utime - start.tms_utime;
+  cout << "sec: " << (double)clocks / clocks_per_sec << endl;
+  return 0;
+}
 
 double CalculateIntegral(int num_of_interval) {
   double a = 0;
