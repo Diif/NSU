fc410d888b35fec5a4f2fe3551a0f793ab89dc9d
Diif <30970636+Diif@users.noreply.github.com>
2021-11-12 22:28:58 +0700

    T3

--- ECD/T3/prog0.s
+++ ECD/T3/prog0.s
@@ -21,17 +21,17 @@ _Z5func1PdS_:
 	cltq # convert long to quad
 	leaq	0(,%rax,8), %rdx # rdx = i * 8
 	movq	-40(%rbp), %rax #  в rax кладет v1[0]
-	leaq	(%rdx,%rax), %rbx # rbx = rdx + rax
+	leaq	(%rdx,%rax), %rbx # rbx = rdx + rax, rbx = v1[i*8]
 	call	rand # берет случайное число, но т.к. зерна нет, то исход один
-	pxor	%xmm0, %xmm0
+	pxor	%xmm0, %xmm0 # v1[i*8] = rand() * const1 / const 2 - const 3
 	cvtsi2sd	%eax, %xmm0 # convert eax to double
-	movsd	.LC0(%rip), %xmm1 # const -> xmm1
+	movsd	.LC0(%rip), %xmm1 # const1 -> xmm1
 	mulsd	%xmm1, %xmm0 # xmm1 * xmm0
 	movsd	.LC1(%rip), %xmm1 # const2 --> xmm1
 	divsd	%xmm1, %xmm0 # xmm0 / xmm1
 	movsd	.LC2(%rip), %xmm1 # const3 --> xmm1
 	subsd	%xmm1, %xmm0 # xmm0 - xmm1;
-	movsd	%xmm0, (%rbx)
+	movsd	%xmm0, (%rbx) # v1[i*8] = result
 	movl	-20(%rbp), %eax # i--> eax
 	cltq
 	leaq	0(,%rax,8), %rdx # i * 8 --> rdx
@@ -67,44 +67,44 @@ _Z5func2PdS_:
 	movq	%rsp, %rbp
 .LCFI5:
 	subq	$48, %rsp
-	movq	%rdi, -24(%rbp)
-	movq	%rsi, -32(%rbp)
+	movq	%rdi, -24(%rbp) # v1 -> -24
+	movq	%rsi, -32(%rbp) # v2 -> -32
 	pxor	%xmm0, %xmm0
-	movsd	%xmm0, -8(%rbp)
-	movl	$0, -12(%rbp)
+	movsd	%xmm0, -8(%rbp) # k = 0 -> -8rbp
+	movl	$0, -12(%rbp) # int i = 0
 .L7:
 	cmpl	$49999999, -12(%rbp)
-	jg	.L6
+	jg	.L6 # if i <= 49999999
 	movl	-12(%rbp), %eax
 	cltq
-	leaq	0(,%rax,8), %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movq	(%rax), %rax
-	movq	%rax, -40(%rbp)
-	movsd	-40(%rbp), %xmm0
-	call	sin
-	movapd	%xmm0, %xmm1
-	movsd	.LC4(%rip), %xmm0
-	mulsd	%xmm0, %xmm1
-	movsd	%xmm1, -40(%rbp)
-	movl	-12(%rbp), %eax
+	leaq	0(,%rax,8), %rdx # rdx = i * 8
+	movq	-24(%rbp), %rax # rax = v1
+	addq	%rdx, %rax # rax = v1 + i*8
+	movq	(%rax), %rax # rax = v1[i*8]
+	movq	%rax, -40(%rbp) # -40rbp = v1[i*8]
+	movsd	-40(%rbp), %xmm0 # v1[i*8] -> xmm0
+	call	sin # sin(v1[i*8])
+	movapd	%xmm0, %xmm1 # copy xmm0 -> xmm1
+	movsd	.LC4(%rip), %xmm0 # const1 -> xmm0
+	mulsd	%xmm0, %xmm1 # xmm1 = const1 * xmm1
+	movsd	%xmm1, -40(%rbp) # xmm1 -> -40rbp, -40rbp = sin(v1[i*8]) * const1
+	movl	-12(%rbp), %eax  # eax = i
 	cltq
-	leaq	0(,%rax,8), %rdx
-	movq	-32(%rbp), %rax
-	addq	%rdx, %rax
-	movq	(%rax), %rax
-	movq	%rax, -48(%rbp)
+	leaq	0(,%rax,8), %rdx # rdx = i * 8
+	movq	-32(%rbp), %rax # rax = v2
+	addq	%rdx, %rax # rdx = v2 + i * 8
+	movq	(%rax), %rax # rax = v2[i*8]
+	movq	%rax, -48(%rbp) # v2[i*8] -> -48rbp
 	movsd	-48(%rbp), %xmm0
-	call	cos
-	mulsd	-40(%rbp), %xmm0
-	movsd	-8(%rbp), %xmm1
-	addsd	%xmm1, %xmm0
-	movsd	%xmm0, -8(%rbp)
-	addl	$1, -12(%rbp)
+	call	cos # cos(v2[i*8])
+	mulsd	-40(%rbp), %xmm0 # xmm0 = cos(v2[i*8]) * sin(v1[i*8]) * const1
+	movsd	-8(%rbp), %xmm1 # xmm1 = k
+	addsd	%xmm1, %xmm0 # xmm0 = xmm0 + k
+	movsd	%xmm0, -8(%rbp) # k = k + xmm0
+	addl	$1, -12(%rbp) # i++
 	jmp	.L7
 .L6:
-	movsd	-8(%rbp), %xmm0
+	movsd	-8(%rbp), %xmm0 # return k
 	leave
 .LCFI6:
 	ret
@@ -136,25 +136,25 @@ main:
 	movq	%rdx, %rsi
 	movq	%rax, %rdi
 	call	_Z5func1PdS_ # fun1(v1, v2)
-	movq	-8(%rbp), %rdx
-	movq	-16(%rbp), %rax
-	movq	%rdx, %rsi
-	movq	%rax, %rdi
-	call	_Z5func2PdS_
+	movq	-8(%rbp), %rdx # v2 -> rdx
+	movq	-16(%rbp), %rax # v1 -> rax
+	movq	%rdx, %rsi # v2 -> rsi
+	movq	%rax, %rdi # v1 -> rdi
+	call	_Z5func2PdS_ # get k
 	movq	%xmm0, %rax
-	movq	%rax, -24(%rbp)
-	movq	-24(%rbp), %rax
-	movq	%rax, -40(%rbp)
+	movq	%rax, -24(%rbp) # -24rbp = fun2 = k
+	movq	-24(%rbp), %rax # -24rbp -> rax
+	movq	%rax, -40(%rbp) # double m = k
 	movsd	-40(%rbp), %xmm0
 	movl	$.LC5, %edi
 	movl	$1, %eax
-	call	printf
+	call	printf #printf("\n\n result = %lf", m);
 	movq	-16(%rbp), %rax
 	movq	%rax, %rdi
-	call	_ZdlPv
+	call	_ZdlPv # delete v1
 	movq	-8(%rbp), %rax
 	movq	%rax, %rdi
-	call	_ZdlPv
+	call	_ZdlPv # delete v2
 	movl	$0, %eax
 	leave
 .LCFI9:
@@ -164,8 +164,8 @@ main:
 	.section	.rodata
 	.align 8
 .LC0:
-	.long	0
-	.long	1079574528
+	.long	0 # 00000000000000000000000000000000
+	.long	1079574528 # 01000000010110010000000000000000
 	.align 8
 .LC1:
 	.long	4290772992
