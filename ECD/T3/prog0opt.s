b55e4da3b884c6afb92aa83c52420b65b34bc176
Diif <30970636+Diif@users.noreply.github.com>
2021-11-12 21:07:42 +0700

    T3

---
+++ ECD/T3/prog0opt.s
@@ -0,0 +1,184 @@
+	.file	"prog.cpp"
+	.text
+	.globl	_Z5func1PdS_
+	.type	_Z5func1PdS_, @function
+_Z5func1PdS_:
+.LFB2:
+	pushq	%rbp
+.LCFI0:
+	movq	%rsp, %rbp
+.LCFI1:
+	pushq	%rbx
+	subq	$40, %rsp
+.LCFI2:
+	movq	%rdi, -40(%rbp) # double* v1 --> -40rbp
+	movq	%rsi, -48(%rbp) # double* v2 --> -48rbp
+	movl	$0, -20(%rbp) # int i = 0 ---> -20rbp
+.L3:
+	cmpl	$49999999, -20(%rbp)
+	jg	.L2 # if i < 49999999
+	# movl	-20(%rbp), %eax # do i --> eax
+	# cltq # скорее всего конвертация в long
+	# leaq	0(,%rax,8), %rdx # rdx = i * 8
+	# movq	-40(%rbp), %rax #  в rax кладет адрес v1
+	# leaq	(%rdx,%rax), %rbx # rdx + rax
+	call	rand
+	pxor	%xmm0, %xmm0
+	cvtsi2sd	%eax, %xmm0 # convert eax to double
+	movsd	.LC0(%rip), %xmm1 # const -> xmm1
+	mulsd	%xmm1, %xmm0 # xmm1 * xmm0
+	movsd	.LC1(%rip), %xmm1 # const2 --> xmm1
+	divsd	%xmm1, %xmm0 # xmm0 / xmm1
+	movsd	.LC2(%rip), %xmm1 # const3 --> xmm1
+	subsd	%xmm1, %xmm0 # xmm0 - xmm1;
+	movsd	%xmm0, (%rbx)
+	movl	-20(%rbp), %eax # i--> eax
+	cltq
+	leaq	0(,%rax,8), %rdx # i * 8 --> rdx
+	movq	-48(%rbp), %rax # v2[0] -->rax
+	leaq	(%rdx,%rax), %rbx # rbx = rax + rdx
+	call	rand
+	pxor	%xmm0, %xmm0
+	cvtsi2sd	%eax, %xmm0
+	movsd	.LC0(%rip), %xmm1
+	mulsd	%xmm1, %xmm0
+	movsd	.LC1(%rip), %xmm1
+	divsd	%xmm1, %xmm0
+	movsd	.LC2(%rip), %xmm1
+	subsd	%xmm1, %xmm0
+	movsd	%xmm0, (%rbx)
+	addl	$1, -20(%rbp) # i++
+	jmp	.L3
+.L2: # else
+	movl	$0, %eax
+	addq	$40, %rsp
+	popq	%rbx
+	popq	%rbp
+.LCFI3:
+	ret
+.LFE2:
+	.size	_Z5func1PdS_, .-_Z5func1PdS_
+	.globl	_Z5func2PdS_
+	.type	_Z5func2PdS_, @function
+_Z5func2PdS_:
+.LFB3:
+	pushq	%rbp
+.LCFI4:
+	movq	%rsp, %rbp
+.LCFI5:
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	pxor	%xmm0, %xmm0
+	movsd	%xmm0, -8(%rbp)
+	movl	$0, -12(%rbp)
+.L7:
+	cmpl	$49999999, -12(%rbp)
+	jg	.L6
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	call	sin
+	movapd	%xmm0, %xmm1
+	movsd	.LC4(%rip), %xmm0
+	mulsd	%xmm0, %xmm1
+	movsd	%xmm1, -40(%rbp)
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-32(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movq	%rax, -48(%rbp)
+	movsd	-48(%rbp), %xmm0
+	call	cos
+	mulsd	-40(%rbp), %xmm0
+	movsd	-8(%rbp), %xmm1
+	addsd	%xmm1, %xmm0
+	movsd	%xmm0, -8(%rbp)
+	addl	$1, -12(%rbp)
+	jmp	.L7
+.L6:
+	movsd	-8(%rbp), %xmm0
+	leave
+.LCFI6:
+	ret
+.LFE3:
+	.size	_Z5func2PdS_, .-_Z5func2PdS_
+	.section	.rodata
+.LC5:
+	.string	"\n\n result = %lf"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB4:
+	pushq	%rbp
+.LCFI7:
+	movq	%rsp, %rbp
+.LCFI8:
+	subq	$48, %rsp
+	pxor	%xmm0, %xmm0
+	movsd	%xmm0, -24(%rbp) # -24(%rbp) = 0
+	movl	$400000000, %edi
+	call	_Znam # new 
+	movq	%rax, -16(%rbp) # double v1 = new double[400000000] --> -16rbp
+	movl	$400000000, %edi
+	call	_Znam
+	movq	%rax, -8(%rbp) # double v2 = new double[400000000] --> -8rbp
+	movq	-8(%rbp), %rdx
+	movq	-16(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_Z5func1PdS_ # fun1(v1, v2)
+	movq	-8(%rbp), %rdx
+	movq	-16(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_Z5func2PdS_
+	movq	%xmm0, %rax
+	movq	%rax, -24(%rbp)
+	movq	-24(%rbp), %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	movl	$.LC5, %edi
+	movl	$1, %eax
+	call	printf
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZdlPv
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZdlPv
+	movl	$0, %eax
+	leave
+.LCFI9:
+	ret
+.LFE4:
+	.size	main, .-main
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	0
+	.long	1079574528
+	.align 8
+.LC1:
+	.long	4290772992
+	.long	1105199103
+	.align 8
+.LC2:
+	.long	0
+	.long	1078525952
+	.align 8
+.LC4:
+	.long	3100958126
+	.long	1075678820
+	.section	.eh_frame,"a",@progbits
+.LEFDE1:
+	.ident	"GCC: (Ubuntu 5.5.0-12ubuntu1~16.04) 5.5.0 20171010"
+	.section	.note.GNU-stack,"",@progbits
