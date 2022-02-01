5de15f19e11a7a835782dd510f5c86ffa0de8ca9
Diif <30970636+Diif@users.noreply.github.com>
2022-02-01 13:16:07 +0700

    Java and C++ folders

--- OOP/T0/0b/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kiwul/Documents/projects/NSU/OOP/T0/0b
-# Build directory: C:/Users/kiwul/Documents/projects/NSU/OOP/T0/0b/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
