7f7e28a19d0da21f51d98155d96ba28b3a5035d7
WSL Debian Diif <prozella@DESKTOP-O5EA561.localdomain>
2022-02-23 22:54:34 +0700

    OS T1 upd

--- OS/T1Getopt/main.c
+++ OS/T1Getopt/main.c
@@ -1,9 +1,12 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/resource.h>
 #include <ulimit.h>
 #include <unistd.h>
+
+#define BUF_SIZE 100
 
 extern char** environ;
 
@@ -15,7 +18,10 @@ void PrintProcessIds();
 void PrintCoreSize();
 void PrintCwd();
 void PrintEnviron();
-void SetCoreLimit();
+void SetCoreLimit(char* limit);
+void SetLimit(char* limit);
+void ParseEnvVar(char* arg, char* o_name, char* o_val, int max_name_size,
+                 int max_val_size);
 int IsDigit(char* word);
 
 int main(int argc, char* argv[]) {
@@ -56,9 +62,15 @@ int main(int argc, char* argv[]) {
       case 'v':
         PrintEnviron();
         break;
-      case 'V':
-
-        break;
+      case 'V': {
+        char name[BUF_SIZE];
+        char val[BUF_SIZE];
+        ParseEnvVar(optarg, name, val, BUF_SIZE, BUF_SIZE);
+        if (setenv(name, val, 1) != 0) {
+          fprintf(stderr, "Set env var.\n");
+          exit(1);
+        }
+      } break;
 
       default:
         fprintf(stderr, "Incorrect options.\n");
@@ -97,9 +109,7 @@ void PrintIds() {
       getuid(), getgid(), getegid(), geteuid());
 }
 
-void SetLeader() {
-  setpgid(0, getpgrp());
-}
+void SetLeader() { setpgid(0, getpgrp()); }
 
 void PrintProcessIds() {
   printf(
@@ -138,11 +148,30 @@ void PrintEnviron() {
   }
 }
 
-void SetCoreLimit(char* new_lim) {
+void SetCoreLimit(char* limit) {
   struct rlimit core_lim;
   if (getrlimit(RLIMIT_CORE, &core_lim) == 0) {
+    core_lim.rlim_cur = atol(limit);
+    if (setrlimit(RLIMIT_CORE, &core_lim) != 0) {
+      fprintf(stderr, "Can't set core limit.\n");
+      exit(1);
+    }
   } else {
     fprintf(stderr, "Can't get core limit.\n");
+    exit(1);
+  }
+}
+
+void SetLimit(char* limit) {
+  struct rlimit file_lim;
+  if (getrlimit(RLIMIT_FSIZE, &file_lim) == 0) {
+    file_lim.rlim_cur = atol(limit);
+    if (setrlimit(RLIMIT_FSIZE, &file_lim) != 0) {
+      fprintf(stderr, "Can't set fsize limit.\n");
+      exit(1);
+    }
+  } else {
+    fprintf(stderr, "Can't get fsize limit.\n");
     exit(1);
   }
 }
@@ -156,4 +185,24 @@ int IsDigit(char* word) {
     p++;
   }
   return 1;
+}
+
+void ParseEnvVar(char* arg, char* o_name, char* o_val, int max_name_size,
+                 int max_val_size) {
+  int i;
+  for (i = 0; arg[i] != '=' && arg[i] != '\0'; i++) {
+  }
+  if (arg[i] == '\0') {
+    fprintf(stderr, "Incorrect env var format. Try \"name=val\".\n");
+    exit(1);
+  }
+  arg[i] = '\0';
+  int name_size = strlen(arg);
+  int val_size = strlen(arg + i + 1);
+  if (name_size >= max_name_size || val_size >= max_val_size) {
+    fprintf(stderr, "Env var name (or val) is too long\n");
+    exit(1);
+  }
+  strcpy(o_name, arg);
+  strcpy(o_val, arg + i + 1);
 }
