b41dbe64c6800872d885dcfd86e48bacd8af4773
Dmitrij Bondar <d.bondar1@g.nsu.ru>
2022-04-27 18:59:59 +0700

    T16 solaris fix

--- OS/T16Termios/main.c
+++ OS/T16Termios/main.c
@@ -4,11 +4,11 @@
 int main() {
   printf("Y or N ?\n");
   struct termios term;
-  const int STDIN = 0;
+   const int STDIN = 0;
   tcgetattr(STDIN, &term);
   term.c_lflag &= ~ICANON;
+  term.c_cc[VMIN] = 1;
   tcsetattr(STDIN, TCSANOW, &term);
-  setbuf(stdin, NULL);
   char c;
   scanf("%c", &c);
   printf("\nAnswer: %c.\n", c);
