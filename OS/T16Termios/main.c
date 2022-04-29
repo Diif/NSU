a1b83a874ddb68853ed46b05935e69600bdb9d0c
Dmitrij Bondar <d.bondar1@g.nsu.ru>
2022-04-29 13:33:28 +0700

    OS T16 fix

--- OS/T16Termios/main.c
+++ OS/T16Termios/main.c
@@ -4,12 +4,19 @@
 int main() {
   printf("Y or N ?\n");
   struct termios term;
-   const int STDIN = 0;
-  tcgetattr(STDIN, &term);
-  term.c_lflag &= ~ICANON;
-  term.c_cc[VMIN] = 1;
-  tcsetattr(STDIN, TCSANOW, &term);
-  char c;
-  scanf("%c", &c);
-  printf("\nAnswer: %c.\n", c);
+  const int STDIN = 0;
+  
+  if(!tcgetattr(STDIN, &term)){
+  	term.c_lflag &= ~ICANON;
+  	int omin = term.c_cc[VMIN];
+  	term.c_cc[VMIN] = 1;
+ 	if(!tcsetattr(STDIN, TCSANOW, &term)){;
+  		char c;
+  		scanf("%c", &c);
+  		printf("\nAnswer: %c.\n", c);
+  		term.c_lflag |= ICANON;
+  		term.c_cc[VMIN] = omin;
+		tcsetattr(STDIN, TCSANOW, &term);
+  	}
+  }
 }
