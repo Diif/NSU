71263743eed194c956ffd9a5d576f1d9ebf842a6
prozella <kiwulik@yandex.ru>
2022-05-01 21:48:49 +0700

    BPP T4 is ready

--- BPP/T4GameOfLife/game_of_life_single.c
+++ BPP/T4GameOfLife/game_of_life_single.c
@@ -1,4 +1,3 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdio_ext.h>
 #include <stdlib.h>
@@ -9,7 +8,8 @@
 
 void CreateGame(int max_rows, int max_columns);
 int CreateGameField(char **matrix_main, char **matrix_main_loop,
-                    char **matrix_next, char **matrix_next_loop);
+                    char **matrix_next, char **matrix_next_loop, int max_rows,
+                    int max_cols);
 void GameProcess(char *matrix_main, char *matrix_next, char *matrix_main_loop,
                  char *matrix_next_loop, int max_rows, int max_columns);
 void EndOfGame(int loop_found);
@@ -35,17 +35,16 @@ int CheckDiagonalsBotLeft(char *matrix, int y, int x, int max_rows,
                           int max_columns);
 int CheckDiagonalsBotRight(char *matrix, int y, int x, int max_rows,
                            int max_columns);
-int InitMatrix(char **matrix, int size);
+int InitMatrix(char **matrix, int max_rows, int max_columns);
 int InputMatrix(char *matrix, int max_rows, int max_columns);
-int CheckAndInitMatrix(char **matrix, int size, int no_error);
-void CopyMatrixToMatrixSkipShadowLines(char *source, char *destination,
-                                       int max_rows, int max_columns);
-int CompareMatricesIgnoreShadow(char *matrix1, char *matrix2, int max_rows,
-                                int max_columns);
+int CheckAndInitMatrix(char **matrix, int max_rows, int max_columns,
+                       int no_error);
+void CopyMatrixToMatrix(char *source, char *destination, int max_rows,
+                        int max_columns);
+int CompareMatrices(char *matrix1, char *matrix2, int max_rows,
+                    int max_columns);
 void PrintMatrix(char *matrix, int max_rows, int max_columns);
-void CalculateSizesAndDisplsForScatter(int num_of_proc, int max_rows,
-                                       int max_columns, int **sizes,
-                                       int **displs);
+
 int IsCorrectSymbol(char c);
 void PrintErrorMessage(char *str);
 void PrintEndMessage(char *str);
@@ -56,52 +55,40 @@ void ResetColor();
 
 int MAX_ROWS;
 int MAX_COLUMNS;
-int num_of_proc, rank;
-int *sizes_for_scatter;
-int *displs;
-int shadow_shift;
 
 int main(int argc, char **argv) {
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &num_of_proc);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (argc != 3) {
-    if (!rank) {
-      printf("Not enough args.Call with: ./a.out MAX_ROWS MAX_COLUMNS\n");
-    }
+    printf("Not enough args.Call with: ./a.out MAX_ROWS MAX_COLUMNS\n");
     exit(EXIT_FAILURE);
   }
+
   MAX_ROWS = atoi(argv[1]);
   MAX_COLUMNS = atoi(argv[2]);
   if (MAX_ROWS < 100 || MAX_COLUMNS < 100) {
-    if (!rank) {
-      printf("Field is too small.\n");
-    }
+    printf("Field is too small.\n");
     exit(EXIT_FAILURE);
   }
-
   CreateGame(MAX_ROWS, MAX_COLUMNS);
-  MPI_Finalize();
   return 0;
 }
 
 void CreateGame(int max_rows, int max_colums) {
   char *matrix_main, *matrix_next, *matrix_main_loop, *matrix_next_loop;
   int no_memory_overflow;
-  shadow_shift = max_colums;
 
-  CalculateSizesAndDisplsForScatter(num_of_proc, max_rows, max_colums,
-                                    &sizes_for_scatter, &displs);
-  no_memory_overflow = CreateGameField(&matrix_main, &matrix_main_loop,
-                                       &matrix_next, &matrix_next_loop);
-  int num_procs_ok = 0;
-  MPI_Allreduce(&no_memory_overflow, &num_procs_ok, 1, MPI_INT, MPI_SUM,
-                MPI_COMM_WORLD);
-  if (num_procs_ok == num_of_proc) {
-    int l_max_rows = sizes_for_scatter[rank] / max_colums;
-    InputMatrix(matrix_main, l_max_rows, max_colums);
-    GameProcess(matrix_main, matrix_next, matrix_main_loop, matrix_next_loop,
-                l_max_rows, max_colums);
+  no_memory_overflow =
+      CreateGameField(&matrix_main, &matrix_main_loop, &matrix_next,
+                      &matrix_next_loop, max_rows, max_colums);
+
+  if (no_memory_overflow) {
+    int no_input_error;
+    no_input_error = InputMatrix(matrix_main, max_rows, max_colums);
+    if (no_input_error) {
+      GameProcess(matrix_main, matrix_next, matrix_main_loop, matrix_next_loop,
+                  max_rows, max_colums);
+    } else {
+      PrintErrorMessage("Incorrect level input!");
+    }
   } else {
     PrintErrorMessage("Can't allocate memory!");
   }
@@ -114,46 +101,38 @@ void CreateGame(int max_rows, int max_colums) {
 }
 
 int CreateGameField(char **matrix_main, char **matrix_main_loop,
-                    char **matrix_next, char **matrix_next_loop) {
+                    char **matrix_next, char **matrix_next_loop, int max_rows,
+                    int max_cols) {
   int no_memory_overflow = 1;
-  no_memory_overflow &= CheckAndInitMatrix(matrix_main, sizes_for_scatter[rank],
+  no_memory_overflow &=
+      CheckAndInitMatrix(matrix_main, max_rows, max_cols, no_memory_overflow);
+  no_memory_overflow &= CheckAndInitMatrix(matrix_main_loop, max_rows, max_cols,
                                            no_memory_overflow);
-  no_memory_overflow &= CheckAndInitMatrix(
-      matrix_main_loop, sizes_for_scatter[rank], no_memory_overflow);
-  no_memory_overflow &= CheckAndInitMatrix(matrix_next, sizes_for_scatter[rank],
+  no_memory_overflow &=
+      CheckAndInitMatrix(matrix_next, max_rows, max_cols, no_memory_overflow);
+  no_memory_overflow &= CheckAndInitMatrix(matrix_next_loop, max_rows, max_cols,
                                            no_memory_overflow);
-  no_memory_overflow &= CheckAndInitMatrix(
-      matrix_next_loop, sizes_for_scatter[rank], no_memory_overflow);
   return no_memory_overflow;
 }
 
 void GameProcess(char *matrix_main, char *matrix_next, char *matrix_main_loop,
-                 char *matrix_next_loop, int l_max_rows, int max_columns) {
-  int loop_found = 0, counter = 0;
-  int total_loops = 0;
-  CopyMatrixToMatrixSkipShadowLines(matrix_main, matrix_main_loop, l_max_rows,
-                                    max_columns);
+                 char *matrix_next_loop, int max_rows, int max_columns) {
+  int counter = 0;
+  int loop_found = 0;
+
+  CopyMatrixToMatrix(matrix_main, matrix_main_loop, max_rows, max_columns);
 
   while (!loop_found) {
     counter++;
-    UpdateMain(matrix_main, matrix_next, l_max_rows, max_columns);
+    UpdateMain(matrix_main, matrix_next, max_rows, max_columns);
 
-    UpdateLoop(matrix_main_loop, matrix_next_loop, l_max_rows, max_columns);
+    UpdateLoop(matrix_main_loop, matrix_next_loop, max_rows, max_columns);
 
-    loop_found = CompareMatricesIgnoreShadow(matrix_main, matrix_main_loop,
-                                             l_max_rows, max_columns);
-
-    MPI_Allreduce(&loop_found, &total_loops, 1, MPI_INT, MPI_SUM,
-                  MPI_COMM_WORLD);
-    if (total_loops != num_of_proc) {
-      total_loops = 0;
-      loop_found = 0;
-    }
+    loop_found =
+        CompareMatrices(matrix_main, matrix_main_loop, max_rows, max_columns);
   }
-  if (!rank) {
-    printf("Counter %d.\n", counter);
-    EndOfGame(loop_found);
-  }
+  printf("Counter %d.\n", counter);
+  EndOfGame(loop_found);
 }
 
 void EndOfGame(int loop_found) {
@@ -167,63 +146,26 @@ void EndOfGame(int loop_found) {
 void UpdateMain(char *matrix_main, char *matrix_next, int max_rows,
                 int max_columns) {
   UpdateLevelState(matrix_main, matrix_next, max_rows, max_columns);
-  CopyMatrixToMatrixSkipShadowLines(matrix_next, matrix_main, max_rows,
-                                    max_columns);
+  CopyMatrixToMatrix(matrix_next, matrix_main, max_rows, max_columns);
 }
 
 void UpdateLoop(char *matrix_main_loop, char *matrix_next_loop, int max_rows,
                 int max_columns) {
   UpdateLevelState(matrix_main_loop, matrix_next_loop, max_rows, max_columns);
-  CopyMatrixToMatrixSkipShadowLines(matrix_next_loop, matrix_main_loop,
-                                    max_rows, max_columns);
+  CopyMatrixToMatrix(matrix_next_loop, matrix_main_loop, max_rows, max_columns);
   UpdateLevelState(matrix_main_loop, matrix_next_loop, max_rows, max_columns);
-  CopyMatrixToMatrixSkipShadowLines(matrix_next_loop, matrix_main_loop,
-                                    max_rows, max_columns);
+  CopyMatrixToMatrix(matrix_next_loop, matrix_main_loop, max_rows, max_columns);
 }
 
 void UpdateLevelState(char *matrix_main, char *matrix_next, int max_rows,
                       int max_columns) {
-  int next_rank = rank + 1;
-  int prev_rank = rank - 1;
-  if (prev_rank < 0) {
-    prev_rank = num_of_proc - 1;
-  }
-  if (next_rank >= num_of_proc) {
-    next_rank = 0;
-  }
-  MPI_Request send_first, send_last, rec_first, rec_last;
-
-  MPI_Isend(matrix_main + shadow_shift, max_columns, MPI_CHAR, prev_rank, 0,
-            MPI_COMM_WORLD, &send_first);
-  MPI_Isend(matrix_main + (max_rows * max_columns), max_columns, MPI_CHAR,
-            next_rank, 0, MPI_COMM_WORLD, &send_last);
-  MPI_Irecv(matrix_main, max_columns, MPI_CHAR, prev_rank, 0, MPI_COMM_WORLD,
-            &rec_first);
-  MPI_Irecv(matrix_main + (max_rows * max_columns) + shadow_shift, max_columns,
-            MPI_CHAR, next_rank, 0, MPI_COMM_WORLD, &rec_last);
-  for (int cur_row = 2; cur_row < max_rows; cur_row++) {
+  for (int cur_row = 0; cur_row < max_rows; cur_row++) {
     int shift = cur_row * max_columns;
     for (int cur_column = 0; cur_column < max_columns; cur_column++) {
       matrix_next[shift + cur_column] = UpdateCellState(
           matrix_main, cur_row, cur_column, max_rows, max_columns);
     }
   }
-  MPI_Wait(&rec_first, MPI_STATUS_IGNORE);
-  int cur_row = 1;
-  int shift = cur_row * max_columns;
-  for (int cur_column = 0; cur_column < max_columns; cur_column++) {
-    matrix_next[shift + cur_column] = UpdateCellState(
-        matrix_main, cur_row, cur_column, max_rows, max_columns);
-  }
-  MPI_Wait(&rec_last, MPI_STATUS_IGNORE);
-  cur_row = max_rows;
-  shift = cur_row * max_columns;
-  for (int cur_column = 0; cur_column < max_columns; cur_column++) {
-    matrix_next[shift + cur_column] = UpdateCellState(
-        matrix_main, cur_row, cur_column, max_rows, max_columns);
-  }
-  MPI_Wait(&send_first, MPI_STATUS_IGNORE);
-  MPI_Wait(&send_last, MPI_STATUS_IGNORE);
 }
 
 char UpdateCellState(char *matrix, int y, int x, int max_rows,
@@ -255,21 +197,31 @@ int CalcNeighbours(char *matrix, int y, int x, int max_rows, int max_columns) {
 
 int CheckTop(char *matrix, int y, int x, int max_rows, int max_cols) {
   int count = 0;
-  int last_row = max_rows;
-
-  if (matrix[(y - 1) * max_cols + x] == '*') {
-    count++;
+  int last_row = max_rows - 1;
+  if (y > 0) {
+    if (matrix[(y - 1) * max_cols + x] == '*') {
+      count++;
+    }
+  } else if (y == 0) {
+    if (matrix[last_row * max_cols + x] == '*') {
+      count++;
+    }
   }
-
   return count;
 }
 
 int CheckBot(char *matrix, int y, int x, int max_rows, int max_cols) {
   int count = 0;
-  if (matrix[(y + 1) * max_cols + x] == '*') {
-    count++;
+  int last_row = max_rows - 1;
+  if (y < last_row) {
+    if (matrix[(y + 1) * max_cols + x] == '*') {
+      count++;
+    }
+  } else if (y == last_row) {
+    if (matrix[x] == '*') {
+      count++;
+    }
   }
-
   return count;
 }
 
@@ -306,14 +258,22 @@ int CheckRight(char *matrix, int y, int x, int max_columns) {
 int CheckDiagonalsTopLeft(char *matrix, int y, int x, int max_rows,
                           int max_columns) {
   int count = 0;
-  int last_row = max_rows;
+  int last_row = max_rows - 1;
   int last_column = max_columns - 1;
-  if (x > 0 && y >= 1) {
+  if (x > 0 && y > 0) {
     if (matrix[(y - 1) * max_columns + x - 1] == '*') {
       count++;
     }
-  } else if (x == 0) {
+  } else if (x == 0 && y == 0) {
+    if (matrix[last_row * max_columns + last_column] == '*') {
+      count++;
+    }
+  } else if (x == 0 && y != 0) {
     if (matrix[(y - 1) * max_columns + last_column] == '*') {
+      count++;
+    }
+  } else if (x != 0 && y == 0) {
+    if (matrix[last_row * max_columns + x - 1] == '*') {
       count++;
     }
   }
@@ -323,14 +283,22 @@ int CheckDiagonalsTopLeft(char *matrix, int y, int x, int max_rows,
 int CheckDiagonalsTopRight(char *matrix, int y, int x, int max_rows,
                            int max_columns) {
   int count = 0;
-  int last_row = max_rows;
+  int last_row = max_rows - 1;
   int last_column = max_columns - 1;
-  if (x < last_column && y >= 1) {
+  if (x < last_column && y > 0) {
     if (matrix[(y - 1) * max_columns + x + 1] == '*') {
       count++;
     }
-  } else if (x == last_column) {
+  } else if (x == last_column && y == 0) {
+    if (matrix[last_row * max_columns] == '*') {
+      count++;
+    }
+  } else if (x == last_column && y != 0) {
     if (matrix[(y - 1) * max_columns] == '*') {
+      count++;
+    }
+  } else if (x != last_column && y == 0) {
+    if (matrix[last_row * max_columns + x + 1] == '*') {
       count++;
     }
   }
@@ -340,7 +308,7 @@ int CheckDiagonalsTopRight(char *matrix, int y, int x, int max_rows,
 int CheckDiagonalsBotLeft(char *matrix, int y, int x, int max_rows,
                           int max_columns) {
   int count = 0;
-  int last_row = max_rows + 1;
+  int last_row = max_rows - 1;
   int last_column = max_columns - 1;
   if (x > 0 && y < last_row) {
     if (matrix[(y + 1) * max_columns + x - 1] == '*') {
@@ -365,7 +333,7 @@ int CheckDiagonalsBotLeft(char *matrix, int y, int x, int max_rows,
 int CheckDiagonalsBotRight(char *matrix, int y, int x, int max_rows,
                            int max_columns) {
   int count = 0;
-  int last_row = max_rows + 1;
+  int last_row = max_rows - 1;
   int last_column = max_columns - 1;
   if (x < last_column && y < last_row) {
     if (matrix[(y + 1) * max_columns + x + 1] == '*') {
@@ -387,11 +355,11 @@ int CheckDiagonalsBotRight(char *matrix, int y, int x, int max_rows,
   return count;
 }
 
-int InitMatrix(char **matrix, int size) {
+int InitMatrix(char **matrix, int max_rows, int max_columns) {
   int i;
   int enough_memory = 1;
   *matrix = NULL;
-  *matrix = (char *)malloc((size + shadow_shift * 2) * sizeof(char));
+  *matrix = (char *)malloc(max_rows * max_columns * sizeof(char));
 
   if (*matrix == NULL) {
     enough_memory = 0;
@@ -401,21 +369,19 @@ int InitMatrix(char **matrix, int size) {
 }
 
 int InputMatrix(char *matrix, int max_rows, int max_columns) {
-  memset(matrix, ' ',
-         (max_rows * max_columns + shadow_shift * 2) * sizeof(char));
-  if (!rank) {
-    matrix[1 + shadow_shift] = '*';
-    matrix[max_columns + 2 + shadow_shift] = '*';
-    matrix[max_columns * 2 + shadow_shift] = '*';
-    matrix[max_columns * 2 + 1 + shadow_shift] = '*';
-    matrix[max_columns * 2 + 2 + shadow_shift] = '*';
-  }
+  memset(matrix, ' ', max_columns * max_rows * sizeof(char));
+  matrix[1] = '*';
+  matrix[max_columns + 2] = '*';
+  matrix[max_columns * 2] = '*';
+  matrix[max_columns * 2 + 1] = '*';
+  matrix[max_columns * 2 + 2] = '*';
   return 1;
 }
 
-int CheckAndInitMatrix(char **matrix, int size, int no_error) {
+int CheckAndInitMatrix(char **matrix, int max_rows, int max_columns,
+                       int no_error) {
   if (no_error) {
-    no_error = InitMatrix(matrix, size);
+    no_error = InitMatrix(matrix, max_rows, max_columns);
   }
   return no_error;
 }
@@ -433,16 +399,16 @@ void PrintMatrix(char *matrix, int max_rows, int max_columns) {
   printf("\n");
 }
 
-void CopyMatrixToMatrixSkipShadowLines(char *source, char *destination,
-                                       int max_rows, int max_columns) {
-  memcpy(destination + shadow_shift, source + shadow_shift,
-         max_columns * max_rows);
+void CopyMatrixToMatrix(char *source, char *destination, int max_rows,
+                        int max_columns) {
+  memcpy(destination, source, max_columns * max_rows);
 }
 
-int CompareMatricesIgnoreShadow(char *matrix1, char *matrix2, int max_rows,
-                                int max_columns) {
+int CompareMatrices(char *matrix1, char *matrix2, int max_rows,
+                    int max_columns) {
   int is_equal = 1;
-  for (int i = 1; i < max_rows && is_equal; i++) {
+
+  for (int i = 0; i < max_rows && is_equal; i++) {
     int shift = max_columns * i;
     for (int j = 0; j < max_columns && is_equal; j++) {
       if (matrix1[shift + j] != matrix2[shift + j]) {
@@ -483,24 +449,64 @@ void Cyan() { printf("\033[0;36m"); }
 
 void ResetColor() { printf("\033[0m"); }
 
-void CalculateSizesAndDisplsForScatter(int num_of_proc, int max_rows,
-                                       int max_columns, int **sizes,
-                                       int **displs) {
-  int base = max_rows / num_of_proc;
-  int remainder = max_rows % num_of_proc;
-  *sizes = (int *)malloc(sizeof(int) * num_of_proc);
-  *displs = (int *)malloc(sizeof(int) * num_of_proc);
-  int *p_sizes = *sizes;
-  int *p_dispils = *displs;
-  int offset = 0;
-  for (int i = 0; i < num_of_proc; i++) {
-    p_dispils[i] = offset;
-    p_sizes[i] = base * max_columns;
-    offset += base * max_columns;
-    if (remainder) {
-      p_sizes[i] += max_columns;
-      offset += max_columns;
-      remainder--;
-    }
-  }
-}
+// int Input() {
+//   int res = 0, bytesWaiting;
+//   static const int STDIN = 0;
+//   static int initialized = 0;
+
+//   if (!initialized) {
+//     // Use termios to turn off line buffering
+//     struct termios term;
+//     tcgetattr(STDIN, &term);
+//     term.c_lflag &= ~ICANON;
+//     term.c_lflag &= ~ECHO;
+//     tcsetattr(STDIN, TCSANOW, &term);
+//     initialized = 1;
+//   }
+//   bytesWaiting = 0;
+//   if (ioctl(0, FIONREAD, &bytesWaiting) == 0 && bytesWaiting > 0) {
+//     res = getchar();
+//     // __fpurge(stdin);
+//   }
+//   return res;
+// }
+
+//   FILE *stream = freopen("/dev/pts/2", "r", stdin);
+//   int loop_found = 0, no_life_progress = 0, iRes = 0, speed = 5, counter =
+//   0; struct timeval start, stop;
+
+//   gettimeofday(&start, NULL);
+
+//   CopyMatrixToMatrix(matrix_main, matrix_main_loop, max_rows, max_columns);
+//   PrintMatrix(matrix_main, max_rows, max_columns);
+
+//   while (iRes != 'e' && !loop_found && !no_life_progress) {
+//     iRes = Input();
+//     if ((iRes == '=' || iRes == '+') && speed > 1) speed--;
+//     if (iRes == '-' && speed < 6) speed++;
+//     gettimeofday(&stop, NULL);
+//     if ((((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec -
+//           start.tv_usec) >= 10000 * speed &&
+//          speed < 6) ||
+//         (speed == 6 && iRes == 32)) {
+//       // functions call placeholder start
+//       counter++;
+//       no_life_progress =
+//           UpdateMain(matrix_main, matrix_next, max_rows, max_columns);
+
+//       UpdateLoop(matrix_main_loop, matrix_next_loop, max_rows,
+//       max_columns);
+
+//       loop_found =
+//           CompareMatrices(matrix_main, matrix_main_loop, max_rows,
+//           max_columns);
+//       printf("\e[1;1H\e[2J");
+//       // PrintMatrix(matrix_main, max_rows, max_columns);
+//       // functions call placeholder end
+//       gettimeofday(&start, NULL);
+//     }
+//     printf("\rSpeed:%-i", (6 - speed));
+//   }
+//   printf("Counter value: %d.\n", counter);
+//   EndOfGame(no_life_progress, loop_found);
+// }
