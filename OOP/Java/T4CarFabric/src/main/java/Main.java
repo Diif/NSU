a120f237d54c09ff8de58b3f500b02b9cbf290bf
prozella <kiwulik@yandex.ru>
2022-06-22 15:21:01 +0700

    config path fix

--- OOP/Java/T4CarFabric/src/main/java/Main.java
+++ OOP/Java/T4CarFabric/src/main/java/Main.java
@@ -3,7 +3,7 @@ import model.GameModel;
 import view.GUI;
 
 public class Main {
-    private static final String CONFIG_PATH="resources/config.cfg";
+    private static final String CONFIG_PATH="config.cfg";
     public static void main(String[] argc){
         GameModel model = new GameModel();
         Controller controller = new Controller(model);
