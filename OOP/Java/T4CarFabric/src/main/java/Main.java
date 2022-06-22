44df161493c1ae5184e7a8bf8de07d1e86028979
prozella <kiwulik@yandex.ru>
2022-06-22 12:13:53 +0700

    Config fix

--- OOP/Java/T4CarFabric/src/main/java/Main.java
+++ OOP/Java/T4CarFabric/src/main/java/Main.java
@@ -3,10 +3,11 @@ import model.GameModel;
 import view.GUI;
 
 public class Main {
+    private static final String CONFIG_PATH="resources/config.cfg";
     public static void main(String[] argc){
         GameModel model = new GameModel();
         Controller controller = new Controller(model);
-        model.loadConfig("config.txt");
+        model.loadConfig(CONFIG_PATH);
         model.initModel();
         model.startProduction();
         GUI.startGUI(model,controller);
